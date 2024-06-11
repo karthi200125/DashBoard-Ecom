00cb6aaf1a7e416f09fdacfc15d6d18288f68395
Kathick <skarthikeyan25052001@gmail.com>
2024-06-11 14:15:03 +0530

    complete notfound cutome code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import Categories from "./_components/LandingCategories";
 import Banners from "./_components/LandingPage/Banners";
 import LandingCards from "./_components/LandingPage/LadingCards";
 import LandingPage from "./_components/LandingPage/LandingPage";
+import NotFound from "./_components/NotFound";
 
 export default function Home() {
   
@@ -11,6 +12,7 @@ export default function Home() {
       <LandingPage />
       <Categories />
       <Banners />
+      <NotFound/>
       <LandingCards />
     </main>
   );
