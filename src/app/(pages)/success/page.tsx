bd16d38d28204406af64e26472f63d46fe4d1b29
Kathick <skarthikeyan25052001@gmail.com>
2024-06-27 17:55:34 +0530

    finally coeplet stripe checkout

--- src/app/(pages)/success/page.tsx
+++ src/app/(pages)/success/page.tsx
@@ -1,8 +1,92 @@
-import React from 'react'
+'use client'
+
+import { useCurrentUser } from "@/app/hooks/useCurrentUser"
 
 const Success = () => {
+
+    const user = useCurrentUser()
+
     return (
-        <div>Success</div>
+        <div className="w-full min-h-[90vh] flex flex-col gap-3 py-5">
+            <h3 className="py-3 border-b">Order Information</h3>
+            <div>
+                <h2>desc 21 2024</h2>
+                <h2>{`${user?.address}, ${user?.city}, ${user?.state}, India`}</h2>
+            </div>
+
+            {/* payamnet summary */}
+            <div className="flex flex-col gap-3">
+                <h2>Payment Summary</h2>
+                <p>short report of payment status</p>
+                <div className="max-w-max border rounded-[10px] flex flex-row px-10 py-5 mt-3 gap-10">
+                    <div>
+                        <p>Amount Total</p>
+                        <h2>1000</h2>
+                    </div>
+                    <div>
+                        <p>Amount paid</p>
+                        <h2>1000</h2>
+                    </div>
+                </div>
+            </div>
+
+            {/* products */}
+            <div>
+                <h3>ordered Products</h3>
+                <div className="flex flex-col gap-2 overflow-y-auto">
+                    {/* product */}
+                    <div className="border rounded-[10px] p-5 flex flex-row items-center justify-between">
+                        <div className="">
+                            <h2>product name</h2>
+                            <p>product description</p>
+                        </div>
+                        <div className="flex flex-row items-center ">
+                            <h2 className="min-w-[100px]">
+                                price
+                            </h2>
+                            <h2 className="w-[100px]">
+                                qunaityt
+                            </h2>                            
+                        </div>
+                    </div>
+                    <div className="border rounded-[10px] p-5 flex flex-row items-center justify-between">
+                        <div className="">
+                            <h2>product name</h2>
+                            <p>product description</p>
+                        </div>
+                        <div className="flex flex-row items-center ">
+                            <h2 className="min-w-[100px]">
+                                price
+                            </h2>
+                            <h2 className="w-[100px]">
+                                qunaityt
+                            </h2>                            
+                        </div>
+                    </div>
+                    <div className="border rounded-[10px] p-5 flex flex-row items-center justify-between">
+                        <div className="">
+                            <h2>product name</h2>
+                            <p>product description</p>
+                        </div>
+                        <div className="flex flex-row items-center ">
+                            <h2 className="min-w-[100px]">
+                                price
+                            </h2>
+                            <h2 className="w-[100px]">
+                                qunaityt
+                            </h2>                            
+                        </div>
+                    </div>
+
+                    <div className="w-full p-3 rounded-[10px] bg-black text-white flex flex-row items-center justify-between">
+                        <h2>toatl amount</h2>
+                        <h2>1000</h2>
+                    </div>                    
+
+                </div>
+            </div>
+
+        </div>
     )
 }
 
--- src/app/api/webhook/route.ts
+++ src/app/api/webhook/route.ts
@@ -1,72 +1,84 @@
+import Stripe from 'stripe';
 import { db } from '@/lib/db';
-import { headers } from 'next/headers';
-import { Stripe } from 'stripe';
+import { NextRequest, NextResponse } from 'next/server';
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-    apiVersion: "2024-04-10",
+    apiVersion: '2024-04-10',
     typescript: true
 });
 
-export async function POST(req: Request) {
-    const body = await req.text();
-    const signature = headers().get('Stripe-Signature') as string;
+async function getCartItems(line_items: Stripe.ApiList<Stripe.LineItem>) {
+    const cartItems = await Promise.all(line_items.data.map(async (item) => {
+        const product = await stripe.products.retrieve(item.price.product as string);
+        const productId = product.metadata.productId;
 
-    let event: Stripe.Event;
+        return {
+            product: productId,
+            name: product.name,
+            price: item.price.unit_amount / 100,
+            quantity: item.quantity,
+            image: product.images[0]
+        };
+    }));
+    return cartItems;
+}
 
+export async function POST(req: NextRequest) {
     try {
-        event = stripe.webhooks.constructEvent(
-            body,
-            signature,
-            process.env.STRIPE_WEBHOOK_SECRET!
-        );
-    } catch (error) {
-        console.error('Error verifying Stripe webhook signature:', error);
-        return new Response('Webhook signature verification failed', { status: 400 });
-    }
+        const signature = req.headers.get('stripe-signature');
+        const rawBody = await req.text();
 
-    const session = event.data.object as Stripe.Checkout.Session;
-
-    if (event.type === "checkout.session.completed") {
-        const userId = session.client_reference_id;
-
-        if (!session.metadata || !session.metadata.productIds || !session.metadata.quantities) {
-            console.error('Missing metadata in session:', session.metadata);
-            return new Response('Missing metadata in session', { status: 400 });
-        }
-
-        const productIds = session.metadata.productIds.split(',');
-        const quantities = session.metadata.quantities.split(',').map((q: string) => parseInt(q, 10));
-        const totalPrice = session.amount_total / 100;
-
-        console.log("in route", userId, productIds, quantities, totalPrice);
-
+        let event;
         try {
-            // Assuming productIds and quantities are corresponding arrays
-            const orders = productIds.map((productId, index) => ({
-                userId,
-                productId,
-                quantity: quantities[index],
-                total: totalPrice,
-                status: 'pending',
-            }));
-
-            // Creating orders using Prisma batch create
-            const createdOrders = await db.order.createMany({
-                data: orders,
-            });
-
-            // Connect orders to the user
-            await db.user.update({
-                where: { id: userId },
-                data: { Orders: { connect: createdOrders.map(order => ({ id: order.id })) } },
-            });
-
-            return new Response(JSON.stringify({ success: 'Orders created successfully' }), { status: 200 });
-        } catch (error) {
-            console.error('Error creating orders:', error);
-            return new Response(JSON.stringify({ error: 'Failed to create orders' }), { status: 500 });
+            event = stripe.webhooks.constructEvent(
+                rawBody,
+                signature!,
+                process.env.STRIPE_WEBHOOK_SECRET!
+            );
+        } catch (err) {
+            console.error('Webhook signature verification failed.', err.message);
+            return new NextResponse('Webhook error: Invalid signature', { status: 400 });
         }
-    }
 
-    return new Response('Unhandled event type', { status: 400 });
+        if (event.type === 'checkout.session.completed') {
+            const session = event.data.object as Stripe.Checkout.Session;
+
+            const line_items = await stripe.checkout.sessions.listLineItems(session.id);
+            const cartItems = await getCartItems(line_items);
+
+            const productIds = cartItems.map(item => item.product);
+            const quantities = cartItems.map(item => item.quantity);
+            const totalPrice = cartItems.reduce((total, item) => total + item.price * item.quantity, 0);
+
+            const userId = session.client_reference_id!;
+
+            try {
+                const newOrder = await db.order.create({
+                    data: {
+                        userId,
+                        productsIds: productIds,
+                        quantity: quantities.reduce((acc, q) => acc + q, 0),
+                        total: totalPrice,
+                        status: 'pending',
+                    },
+                });
+
+                // Update user model to connect the new order
+                await db.user.update({
+                    where: { id: userId },
+                    data: { Orders: { connect: { id: newOrder.id } } },
+                });
+
+                return new NextResponse('Order created successfully', { status: 200 });
+            } catch (error) {
+                console.error('Failed to create order:', error);
+                return new NextResponse('Failed to create order', { status: 500 });
+            }
+        }
+
+        return new NextResponse('Unhandled event type', { status: 400 });
+    } catch (error) {
+        console.error('Webhook error:', error);
+        return new NextResponse('Webhook error', { status: 400 });
+    }
 }
