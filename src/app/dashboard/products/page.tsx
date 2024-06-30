e5c62ded2f8865ea1d631c90e3295ca7000aabaa
Kathick <skarthikeyan25052001@gmail.com>
2024-06-30 20:21:54 +0530

    chnages in products page

--- src/app/dashboard/products/page.tsx
+++ src/app/dashboard/products/page.tsx
@@ -1,52 +1,52 @@
-// 'use client'
+'use client'
 
-// import dynamic from "next/dynamic"
-// import { useEffect, useState } from 'react'
-// import { FaUsers } from "react-icons/fa"
-// import { MostLikedProducts } from "../../../../actions/dashboard/dashProduct"
+import dynamic from "next/dynamic"
+import { useEffect, useState } from 'react'
+import { FaUsers } from "react-icons/fa"
+import { MostLikedProducts } from "../../../../actions/dashboard/dashProduct"
 
-// const TopList = dynamic(() => import("@/app/_components/TopList"), { ssr: false })
-// const ProductTable = dynamic(() => import("./ProductTable"), { ssr: false })
-// const TotalProducts = dynamic(() => import("./TotalProducts"), { ssr: false })
-// const SaleCards = dynamic(() => import("./SaleCards"), { ssr: false })
-// const ProductMainChart = dynamic(() => import("./ProductMainChart"), { ssr: false })
+const TopList = dynamic(() => import("@/app/_components/TopList"), { ssr: false })
+const ProductTable = dynamic(() => import("./ProductTable"), { ssr: false })
+const TotalProducts = dynamic(() => import("./TotalProducts"), { ssr: false })
+const SaleCards = dynamic(() => import("./SaleCards"), { ssr: false })
+const ProductMainChart = dynamic(() => import("./ProductMainChart"), { ssr: false })
 
-// const Products = () => {
-//     const [products, setProducts] = useState([])
+const Products = () => {
+    const [products, setProducts] = useState([])
 
-//     useEffect(() => {
-//         const get = async () => {
-//             const response = await MostLikedProducts()
-//             console.log(response)
-//             if (response.success) {                
-//                 setProducts(response.data)
-//             } 
-//         }
-//         get()
-//     }, [])
+    useEffect(() => {
+        const get = async () => {
+            const response = await MostLikedProducts()
+            console.log(response)
+            if (response.success) {                
+                setProducts(response.data)
+            } 
+        }
+        get()
+    }, [])
 
-//     return (
-//         <div className="w-full h-full flex flex-col gap-3 mt-5">
-//             <div className="flex flex-row gap-3">
-//                 <div className="w-[76%] h-full flex flex-col gap-3">
-//                     <div className="flex flex-row items-center h-[300px] w-full gap-3">
-//                         {/* <TotalProducts /> */}
-//                         <SaleCards />
-//                         <div className="w-full border bg-white rounded-[20px] p-5 h-full">
-//                             {/* Add any additional content here */}
-//                         </div>
-//                     </div>
-//                     <div className="border rounded-[20px] bg-white h-[350px]">
-//                         {/* <ProductMainChart /> */}
-//                     </div>
-//                 </div>
-//                 <div className="bg-white border rounded-[20px] w-[23%] p-5 h-[660px] overflow-y-hidden">
-//                     <TopList title="Top Products" icon={<FaUsers size={20} />} data={products} route="products" />
-//                 </div>
-//             </div>
-//             {/* <ProductTable /> */}
-//         </div>
-//     )
-// }
+    return (
+        <div className="w-full h-full flex flex-col gap-3 mt-5">
+            <div className="flex flex-row gap-3">
+                <div className="w-[76%] h-full flex flex-col gap-3">
+                    <div className="flex flex-row items-center h-[300px] w-full gap-3">
+                        <TotalProducts />
+                        <SaleCards />
+                        <div className="w-full border bg-white rounded-[20px] p-5 h-full">
+                            {/* Add any additional content here */}
+                        </div>
+                    </div>
+                    <div className="border rounded-[20px] bg-white h-[350px]">
+                        <ProductMainChart />
+                    </div>
+                </div>
+                <div className="bg-white border rounded-[20px] w-[23%] p-5 h-[660px] overflow-y-hidden">
+                    <TopList title="Top Products" icon={<FaUsers size={20} />} data={products} route="products" />
+                </div>
+            </div>
+            <ProductTable />
+        </div>
+    )
+}
 
-// export default Products
+export default Products
