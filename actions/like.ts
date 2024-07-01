68277f20ec9d2295e7f327eff5246f8f5b92cb46
Kathick <skarthikeyan25052001@gmail.com>
2024-07-01 15:09:31 +0530

    solve like.ts

--- actions/like.ts
+++ actions/like.ts
@@ -4,42 +4,70 @@ import { db } from '@/lib/db';
 import { getUserById } from './users';
 import { getSingleProduct } from './product';
 
+interface User {
+    id: string;
+    favorite: string[];
+}
+
+interface Product {
+    id: string;
+    proName: string | null;
+    proDesc: string | null;
+    proPrice: string | null;
+    proImage: string[];
+    proCategory: string | null;
+    proSubCategory: string | null;
+    isProAvailable: string | null;
+    likes: string[];
+    createdAt: Date;
+    updatedAt: Date;
+}
+
+interface Error {
+    error: string;
+}
+
 export const likeAction = async (productId: string, userId: string) => {
-    const user = await getUserById(userId);
-    const product = await getSingleProduct(productId);
-    if (!user) {
+    const user = await getUserById(userId) as User | Error;
+    const product = await getSingleProduct(productId) as Product | Error;
+
+    if ('error' in user) {
         return { error: "You are not authorized" };
+    }
+
+    if ('error' in product) {
+        return { error: "Product not found" };
     }
 
     try {
         const isLiked = user.favorite.includes(productId);
-        const isProHasLikesUserId = product?.likes?.includes(userId)
-        
-        if (!isLiked && !isProHasLikesUserId) {            
+        const isProHasLikesUserId = product.likes.includes(userId);
+
+        if (!isLiked && !isProHasLikesUserId) {
             const updatedFavorites = [...user.favorite, productId];
 
             await db.user.update({
                 where: { id: userId },
                 data: { favorite: updatedFavorites }
             });
-            
-            const updatedLikes = [...product?.likes, userId];
+
+            const updatedLikes = [...product.likes, userId];
 
             await db.product.update({
                 where: { id: productId },
                 data: { likes: updatedLikes }
             });
 
-            return { success: "Product has been liked" };        
-        } else {            
+            return { success: "Product has been liked" };
+        } else {
             const updatedFavorites = user.favorite.filter(fav => fav !== productId);
 
             await db.user.update({
                 where: { id: userId },
                 data: { favorite: updatedFavorites }
             });
-            
-            const updatedLikes = product?.likes?.filter(like => like !== userId);
+
+            const updatedLikes = product.likes.filter(like => like !== userId);
 
             await db.product.update({
                 where: { id: productId },
