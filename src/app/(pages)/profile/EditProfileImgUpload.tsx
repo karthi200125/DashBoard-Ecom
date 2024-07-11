82f1ab2e1f08d65efbcbe4bdaa902fcf4937ce9d
karthi200125 <skarthikeyan25052001@gmail.com>
2024-07-11 14:30:15 +0530

    compltr edit prodile

--- src/app/(pages)/profile/EditProfileImgUpload.tsx
+++ src/app/(pages)/profile/EditProfileImgUpload.tsx
@@ -1,46 +1,77 @@
-'use client'
-import Image from '@/components/ui/CustomImage'
-import React, { useState } from 'react'
+'use client';
+import CustomBtn from '@/app/_components/CustomBtn';
+import ProgressBarCon from '@/app/_components/ProgressBar';
+import { useUpload } from '@/app/hooks/UplaodFile';
+import Image from '@/components/ui/CustomImage';
+import React, { useState, useEffect } from 'react';
 import { IoCloudUploadOutline } from 'react-icons/io5';
 
-const EditProfileImgUpload = () => {
+interface ProfileImageUploadProps {
+    user: any;
+    onDownloadUrl: (url: string) => void;
+}
 
+const EditProfileImgUpload: React.FC<ProfileImageUploadProps> = ({ user, onDownloadUrl }) => {
+    const [file, setFile] = useState<File | null>(null);
     const [productImage, setProductImage] = useState<string | null>(null);
-    const imguplaod = true;
 
-    const handleImageUpload = (e: any) => {
-        if (e.target.files && e.target.files[0]) {
-            setProductImage(URL.createObjectURL(e.target.files[0]));
+    const { per, UploadFile, downloadUrl } = useUpload({ file });
+
+    const handleUpload = () => {
+        if (file) {
+            UploadFile();
         }
     };
 
-    const per = 30
+    useEffect(() => {
+        if (downloadUrl) {
+            setFile(null);
+            onDownloadUrl(downloadUrl);
+        }
+    }, [downloadUrl]);
 
-    const imageper = 100 - per
+    const handleImageUpload = (e: React.ChangeEvent<HTMLInputElement>) => {
+        if (e.target.files && e.target.files[0]) {
+            setProductImage(URL.createObjectURL(e.target.files[0]));
+            setFile(e.target.files[0]);
+        }
+    };
+
+    const percentage = Number(per);
+    const imagePer = 100 - percentage;
 
     return (
         <div className='w-full flex flex-col gap-3'>
             <h5>Profile Image</h5>
-            <div className='flex flex-row items-center gap-10 h-full flex-1 '>
-                <input type="file" accept="image/*" className="hidden" id="imageupload" onChange={handleImageUpload} />
+            <div className='flex flex-col gap-3'>
+                <div className='flex flex-row items-center gap-10 h-full flex-1'>
+                    <input type="file" accept="image/*" className="hidden" id="imageupload" onChange={handleImageUpload} />
 
-                <label htmlFor="imageupload" className='h-[200px] w-[300px] relative overflow-hidden rounded-[10px]'>
-                    <div className={`absolute bottom-0 left-0 w-full `} style={{ height: `${imageper}%` }}>
-                        <div className={`${per > 10 && "wave wave1 h-[20%]"}`}></div>
-                        <div className='w-full h-[100%] bg-[rgba(0,0,0,0.8)] '></div>
+                    <label htmlFor="imageupload" className='h-[200px] w-[300px] relative overflow-hidden rounded-[10px]'>
+                        <div className={`absolute bottom-0 left-0 w-full`} style={{ height: `${imagePer}%` }}>
+                            {percentage > 10 && <div className="wave wave1 h-[20%]" />}
+                            <div className='w-full h-[100%] bg-[rgba(0,0,0,0.8)]'></div>
+                        </div>
+                        <Image src={productImage ? productImage : (user?.image ? user.image : "")} imgclass="w-[300px] h-[200px] bg-neutral-200 rounded-[10px]" alt='ProfileImage' />
+                    </label>
+                </div>
+                {productImage ?
+                    <div className='flex flex-col w-full gap-1'>
+                        <CustomBtn onClick={handleUpload} btnCls='bg-blue-400 text-white'>Upload Image</CustomBtn>
+                        {per && <ProgressBarCon per={percentage} />}
                     </div>
-                    <Image src={productImage || ''} imgclass="w-[300px] h-[200px] bg-neutral-200 rounded-[10px] " alt='ProfileImage'/>
-                </label>
+                    :
+                    <label
+                        htmlFor="imageupload"
+                        className="flex flex-row gap-3 items-center justify-center border p-3 rounded-[10px] cursor-pointer hover:bg-neutral-100 w-full"
+                    >
+                        <IoCloudUploadOutline size={25} />
+                        <h5>Select image</h5>
+                    </label>
+                }
             </div>
-            <label
-                htmlFor="imageupload"
-                className="flex flex-row gap-3 items-center justify-center border p-3 rounded-[10px] cursor-pointer hover:bg-neutral-100 w-full"
-            >
-                <IoCloudUploadOutline size={25} />
-                <h5>Select image</h5>
-            </label>
         </div>
-    )
-}
+    );
+};
 
-export default EditProfileImgUpload
+export default EditProfileImgUpload;
--- src/app/_components/Modal/EditProfileModel.tsx
+++ src/app/_components/Modal/EditProfileModel.tsx
@@ -6,7 +6,7 @@ import { cities, states } from '@/app/_components/dummydata';
 import { useCurrentUser } from '@/app/hooks/useCurrentUser';
 import useEditProfileModal from '@/app/hooks/useEditProfileModel';
 import { zodResolver } from '@hookform/resolvers/zod';
-import { useTransition } from 'react';
+import { useState, useTransition } from 'react';
 import { FormProvider, useForm } from 'react-hook-form';
 import { toast } from 'sonner';
 import { z } from 'zod';
@@ -23,6 +23,8 @@ const EditProfile = () => {
     const editprofilemodel = useEditProfileModal()
     const router = useRouter()
     const queryClient = useQueryClient()
+    const [profileImg, setprofileImg] = useState('')
+    console.log(profileImg)
 
     const methods = useForm<z.infer<typeof UserSchema>>({
         resolver: zodResolver(UserSchema),
@@ -30,9 +32,11 @@ const EditProfile = () => {
             email: user?.email || '',
             name: user?.name || '',
             address: user?.address || '',
+            image: profileImg ? profileImg : user?.image,
             city: user?.city || '',
             state: user?.state || '',
             phoneNo: user?.phoneNo || '',
+            gender: user?.gender || '',
             postalCode: user?.postalCode || ''
         }
     });
@@ -41,9 +45,12 @@ const EditProfile = () => {
         const data = {
             ...values,
             id: user?.id,
-            image: ""
         }
+        console.log(values)
         startTransition(() => {
+            if (!user?.image || !profileImg) {
+                toast.error("select your profile picture")
+            }
             updateUser(data)
                 .then((data) => {
                     if (data.success) {
@@ -62,14 +69,14 @@ const EditProfile = () => {
     return (
         <FormProvider {...methods}>
             <form onSubmit={methods.handleSubmit(handleSubmit)}>
-                <div className='w-full md:w-[1000px] p-2 md:p-5 overflow-y-auto'>
+                <div className='w-full  p-2 md:p-5 overflow-y-auto'>
                     <div className='py-2 border-b'>
                         <h4>Edit Profile</h4>
                         <p>Here you can edit public information about yourself</p>
                     </div>
-                    <div className='flex flex-col md:flex-row gap-5 justify-between'>
-                        <div className='w-full md:w-[300px] flex flex-col gap-3 py-3'>
-                            <EditProfileImgUpload />
+                    <div className='flex flex-col xl:flex-row gap-5 justify-between overflow-y-auto'>
+                        <div className='w-full xl:w-[300px] flex flex-col md:flex-row xl:flex-col gap-3 py-3'>
+                            <EditProfileImgUpload user={user} onDownloadUrl={(d: any) => setprofileImg(d)} />
                             <CustomBtn arrow btnCls='w-full border bg-black text-white' arrowCls='bg-white text-black' isLoading={isLoading} onClick={methods.handleSubmit(handleSubmit)}>Update Profile</CustomBtn>
                         </div>
                         <div className='w-full md:w-[650px] flex flex-wrap gap-5 justify-between overflow-y-auto py-3'>
@@ -77,6 +84,7 @@ const EditProfile = () => {
                             <CustomInput name='email' inputCls='flex-1' label='Email' type='email' isLoading={isLoading} />
                             <CustomInput name='phoneNo' inputCls='flex-1' label='Mobile Number' type='number' isLoading={isLoading} />
                             <CustomInput name='address' inputCls='flex-1' label='Permanent Address' type='text' isLoading={isLoading} />
+                            <CustomInput name='gender' inputCls='flex-1' label='your gender' type='text' isLoading={isLoading} />
                             <CustomInput name='postalCode' inputCls='flex-1' label='Postal Code' type='number' isLoading={isLoading} />
                             <div className='flex flex-col md:flex-row items-center gap-5 w-full'>
                                 <CustomSelect
@@ -85,7 +93,7 @@ const EditProfile = () => {
                                     defaultValue={methods.getValues("city")}
                                     options={cities}
                                     label="Select City"
-                                    selectCls="w-full"                                    
+                                    selectCls="w-full"
                                 />
                                 <CustomSelect
                                     name="state"
@@ -93,7 +101,7 @@ const EditProfile = () => {
                                     defaultValue={methods.getValues("state")}
                                     options={states}
                                     label="Select State"
-                                    selectCls="w-full"                                    
+                                    selectCls="w-full"
                                 />
                             </div>
                         </div>
@@ -112,7 +120,7 @@ const EditProfileModel = () => {
             isOpen={editprofilemodel.isOpen}
             toggleOpen={editprofilemodel.onClose}
             modalBody={<EditProfile />}
-            modalCls=''
+            modalCls='w-full xl:w-[1000px]'
         />
     )
 }
