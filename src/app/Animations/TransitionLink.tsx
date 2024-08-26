79bfa36877f62ca1701547dbe65274f07b4d6a53
Karthi Keyan <125793976+karthi200125@users.noreply.github.com>
2024-08-26 10:14:06 +0530

    some chnages in transtion link

--- src/app/Animations/TransitionLink.tsx
+++ src/app/Animations/TransitionLink.tsx
@@ -1,23 +1,35 @@
 'use client';
 
 import { usePathname, useRouter } from 'next/navigation';
-import React from 'react';
-import { animatePageOut } from './pageTransistionAnimate';
-
+import React, { MouseEvent, useEffect, useState } from 'react';
+import { animatePageOut, animatePageIn } from './pageTransistionAnimate';
 
 interface Props {
     href?: string;
     children: React.ReactNode;
 }
 
-const TransitionLink = ({ href, children }: Props) => {
+const TransitionLink: React.FC<Props> = ({ href, children }) => {
     const router = useRouter();
     const pathname = usePathname();
+    const [isAnimating, setIsAnimating] = useState(false);
 
-    const handleClick = () => {
-        if (href && pathname !== href) {
-            animatePageOut(href, router);
-            router.push(href)
+    useEffect(() => {
+        // Handle page entry animation when the component mounts or the path changes
+        animatePageIn();
+    }, [pathname]);
+
+    const handleClick = async (e: MouseEvent<HTMLDivElement>) => {
+        e.preventDefault();
+        if (!href || pathname === href || isAnimating) return;
+
+        setIsAnimating(true);
+
+        try {
+            await animatePageOut(href, router);
+            router.push(href);
+        } catch (error) {           
+            setIsAnimating(false);
         }
     };
 
